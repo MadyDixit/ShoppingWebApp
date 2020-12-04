f361f2bde310b5b3f581eae1d0768101b0fcd9e7
Madhav-Laptop <madhav0404dixit@gmail.com>
2020-12-04 11:05:56 +0530

    SuccessMsg In cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public showMsg: String = '';
   /**
    * Cart
    */
   public Cart() {
-    
+      this.showMsg = "Successfully Inserted";
   }
   title = 'Flipkart';
 }
