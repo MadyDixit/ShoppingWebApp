0e3d98cbf6f198e9bd9c3a92a002773cd365e459
Madhav-Laptop <madhav0404dixit@gmail.com>
2020-12-04 11:15:11 +0530

    Update Comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public showMsg: String = '';
   /**
-   * Cart
+   * Cart Into Web Application
    */
   public Cart() {
       this.showMsg = "Successfully Inserted";
